352d7d952b7ce89f137a09d0c0e0edd8de5dfcab
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-09-13 16:14:55 +0200

    Fix

--- experiments/github_react_agent.py
+++ experiments/github_react_agent.py
@@ -5,19 +5,20 @@ from github_tools import github_issues_tools
 from langchain.prompts import PromptTemplate
 
 class GithubReActAgent(ReActAgent):
-    PROMPT_TEMPLATE="""Answer the following questions as best you can. You have access to the following tools:
+    PROMPT_TEMPLATE="""You are a helpful, respectful and honest Github Agent assistant. You can help the user to manage the issues in his repositories.
+You have access to the following tools to achieve this goal:
 {tools}
 
-Use the following format:
+To answer the question you can use the following format:
 
 Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action. Use the character "|" to separate the parameters.
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+Thought: you should always think about what to do, usually it is something related to GitHub issues (i.e. create an issue, update an issue status, etc.)
+Action: the action to take, should be one of the following tools [{tool_names}]. Don't use backticks around the tool name.
+Action Input: the input to the action. It is a list of parameters separated by the "|" character.
+Observation: the result of the action. If the action is successfully or an error occurred exit immediately with the result for the user.
+... (only in the case of multiple actions requested by the user this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer and I prepare the right output to show to the user.
+Final Answer: the final answer to the original input question.
 
 Begin!
 
